e4891e7c06a46fd8b10cd57e525433cfab9134c4
marlenetiare <marlene.jamett@gmail.com>
2015-11-13 17:14:52 -0300

    update Form contact

--- prod/js/main.js
+++ prod/js/main.js
@@ -34,5 +34,40 @@ $(document).ready(function() {
 			theme:'minimal',
 		});
 	});
+
+
+	var $elementAnima = $('#social-recomendacion');//elemento para animar
+	var $animation_elements = $elementAnima;
+	var $window = $(window);
+	function check_if_in_view(){
+		
+		$.each($animation_elements, function(){
+
+			var window_top_position = $window.scrollTop();
+		
+			if(document.documentElement.clientWidth >= 481){
+
+				if(window_top_position > 150){//ScrollTop PX
+					$animation_elements.removeClass('hide-social').addClass('show-social');
+				}
+				if(window_top_position < 150){//ScrollTop PX
+					$animation_elements.removeClass('show-social').addClass('hide-social');
+				}
+			}
+			if(document.documentElement.clientWidth <= 480){
+
+				if(window_top_position > 60){//ScrollTop PX
+					$animation_elements.removeClass('hide-social').addClass('show-social');
+				}
+				if(window_top_position < 60){//ScrollTop PX
+					$animation_elements.removeClass('show-social').addClass('hide-social');
+				}
+			}	
+
+		});
+	}
+	$window.on('scroll resize', check_if_in_view);
+	$window.trigger('scroll');  
 });
+
 
